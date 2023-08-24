36469e1030f2e3ce4f48376b8cc5d82bc335cba1
Brayan Moreno <bramoreno.dev@gmail.com>
2023-08-24 16:16:03 -0500

    initial commit with Spring Initializer

---
+++ src/main/java/com/project/brayan/demob/DemobApplication.java
@@ -0,0 +1,15 @@
+package com.project.brayan.demob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobApplication.class, args);
+	}
+
+
+
+}
