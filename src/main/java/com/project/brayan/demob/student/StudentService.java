c907fd9a04ec48450cf53cc6269f5c466fc291cd
Brayan Moreno <bramoreno.dev@gmail.com>
2023-08-24 18:17:08 -0500

    Add get, post, put, and delete queries

--- src/main/java/com/project/brayan/demob/student/StudentService.java
+++ src/main/java/com/project/brayan/demob/student/StudentService.java
@@ -1,32 +1,60 @@
 package com.project.brayan.demob.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
-    @GetMapping
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
     public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
 
-        Student student1 =new Student(
-                123L,
-                "Andres",
-                "gda@gmail.com",
-                LocalDate.of(1998,5,14),
-                26
-        );
+    public void addStudent(Student student) {
+        studentRepository.save(student);
+    }
 
-        Student student2 =new Student(
-                1234L,
-                "Fernanda",
-                "fer@gmail.com",
-                LocalDate.of(1999,2,11),
-                24
-        );
+    public Student getStudent(Long studentId) {
+        Optional<Student> optionalStudent = studentRepository.findById(studentId);
+        if (!optionalStudent.isPresent()) {
+            throw new IllegalArgumentException("Id no existe");
+        }
+        return optionalStudent.get();
+    }
 
-        return List.of(student1,student2);
+    public void deleteStudent(Long studentId) {
+        Optional<Student> optionalStudent = studentRepository.findById(studentId);
+        if (!optionalStudent.isPresent()) {
+            throw new IllegalArgumentException("Id no existe");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Optional<Student> optionalStudent = studentRepository.findById(studentId);
+        if (!optionalStudent.isPresent()) {
+            throw new IllegalArgumentException("Id no existe");
+        }
+        Student student = optionalStudent.get();
+        if (name != null && name.length() > 0 && !name.equals(student.getName())) {
+            student.setName(name);
+        }
+        if (email != null && email.length() > 0 && !email.equals(student.getEmail())) {
+            student.setEmail(email);
+        }
+
     }
 }
