b85a4b9124696b1037367992d920317f4b8481d3
Brayan Moreno <bramoreno.dev@gmail.com>
2023-08-24 16:18:38 -0500

    feat: Student Service

---
+++ src/main/java/com/project/brayan/demob/student/StudentService.java
@@ -0,0 +1,32 @@
+package com.project.brayan.demob.student;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class StudentService {
+    @GetMapping
+    @Service
+    public List<Student> getStudents() {
+
+        Student student1 =new Student(
+                123L,
+                "Andres",
+                "gda@gmail.com",
+                LocalDate.of(1998,5,14),
+                26
+        );
+
+        Student student2 =new Student(
+                1234L,
+                "Fernanda",
+                "fer@gmail.com",
+                LocalDate.of(1999,2,11),
+                24
+        );
+
+        return List.of(student1,student2);
+    }
+}
